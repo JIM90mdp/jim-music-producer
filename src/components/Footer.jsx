5ee9e9062546e6b2a2688d796228dca782a2af61
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-12-07 20:49:40 -0300

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base'>
       <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-        <div className='flex items-center lg:py-2'>
+        {/* <div className='flex items-center lg:py-2'>
           Build  by&nbsp;
           <Link href='https://github.com/JIM90mdp' className='underline underline-offset-2' target={'_blank'}>
             JIM-Dev
@@ -14,7 +14,7 @@ const Footer = () => {
         </div>
         <Link href='https://www.linkedin.com/in/mascarenhas-developer/' className='underline underline-offset-2' target={'_blank'}>
           Say hello
-        </Link>
+        </Link> */}
       </Layout>
     </footer>
   );
