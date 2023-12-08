d02f5f195bfd3129a4fb902ea49bba4bb4ff88f7
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-12-07 21:07:54 -0300

    Refactor cv and seo url

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,9 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className='flex items-center justify-center rounded-full font-semibold bg-dark text-light px-6 py-3 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold'
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light px-6 py-3 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -17,41 +17,31 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
-      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm'>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Skills
+      </h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm">
         <motion.div
-          className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2'
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
           Musician
         </motion.div>
 
-        <Skill name='Ableton Live' x='-25vw' y='2vw' />
-        <Skill name='Sibelius' x='-5vw' y='-10vw' />
-        <Skill name='Finale' x='20vw' y='6vw' />
-        <Skill name='Pro Tools' x='0vw' y='12vw' />
-        <Skill name='Reaper' x='-20vw' y='-15vw' />
-        <Skill name='Adobe Audition' x='15vw' y='-12vw' />
-        <Skill name='Active learning' x='28vw' y='-5vw' />
-        <Skill name='Teamwork' x='0vw' y='-20vw' />
-        <Skill name='Performance' x='-15vw' y='-3vw' />
-        <Skill name='Improvisation' x='-10vw' y='18vw' />
-        <Skill name='Musical Theory' x='15vw' y='15vw' />
-        <Skill name='Arrangements' x='-18vw' y='10vw' />
-        <Skill name='Instrumentation' x='10vw' y='-5vw' />
-        <Skill name='Recording' x='-12vw' y='-9vw' />
-        <Skill name='Compositon' x='-9vw' y='3vw' />
-        <Skill name='Sound Design' x='12vw' y='0vw' />
-        <Skill name='Problem-solving' x='5vw' y='-15vw' />
-        <Skill name='Adaptability' x='25vw' y='1vw' />
-        <Skill name='Teaching' x='9vw' y='9vw' />
+        <Skill name="Ableton Live" x="-25vw" y="2vw" />
+        <Skill name="Sound Design" x="20vw" y="0vw" />
+        <Skill name="Compositon" x="-12vw" y="3vw" />
+        <Skill name="Recording" x="-12vw" y="-9vw" />
+        <Skill name="Teaching" x="9vw" y="9vw" />
+        <Skill name="Sibelius" x="-5vw" y="-15vw" />
+        <Skill name="Reaper" x="-20vw" y="-15vw" />
+        <Skill name="Adobe Audition" x="15vw" y="-12vw" />
       </div>
     </>
   );
 };
 
 export default Skills;
-
 
 // import { motion } from 'framer-motion';
 
@@ -61,7 +51,7 @@ export default Skills;
 //       className='flex items-center justify-center rounded-full font-semibold bg-dark text-light px-6 py-3 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold'
 //       whileHover={{ scale: 1.05 }}
 //       initial={{ x: 0, y: 0 }}
-//       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }} 
+//       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
 //       viewport={{ once: true }}
 //     >
 //       {name}
@@ -74,7 +64,7 @@ export default Skills;
 //     <>
 //       <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
 //       <div className='w-full h-screen relative flex items-center justify-center lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] '>
-  
+
 //         <Skill name='HTML' x='-31vw' y='0vw' />
 //         <Skill name='CSS' x='-26vw' y='0vw' />
 //         <Skill name='JavaScript' x='-19vw' y='0vw' />
